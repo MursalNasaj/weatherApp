a8078bc992847c850d06ea94eba501895e68e063
Mursal Nasaj <nasajmursal@gmail.com>
2023-05-25 23:00:45 -0700

    finish

--- script.js
+++ script.js
@@ -1,51 +1,52 @@
-function formatDate(date){
-    let hours = currentTime.getHours();
-if(hours < 10){
-    hours = `0${hours}`;
-}
-let minutes = currentTime.getMinutes();
-if(minutes < 10){
-    minutes = `0${minutes}`;
-}
-let dayIndex = currentTime.getDay();
-
-let days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
-
-let day = days[dayIndex];
-return `${day} ${hours}: ${minutes};`
-     
-}
-
-function search(event){
+function formatDate(date) {
+    let hours = date.getHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+  
+    let dayIndex = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+    let day = days[dayIndex];
+  
+    return `${day} ${hours}:${minutes}`;
+  }
+  
+  function search(event) {
     event.preventDefault();
     let cityElement = document.querySelector("#city");
-    cityElement.innerHTML=cityInput.value;
-    let cityInput =document.querySelector("#city-input")
-}
-
-function convertFahrenheit(event){
+    let cityInput = document.querySelector("#city-input");
+    cityElement.innerHTML = cityInput.value;
+  }
+  
+  function convertToFahrenheit(event) {
     event.preventDefault();
     let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = 43;
-}
-
-function convertCelsius(event){
+    temperatureElement.innerHTML = 66;
+  }
+  
+  function convertToCelsius(event) {
     event.preventDefault();
     let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = 16;
-}
-
-let dateElement = document.querySelector("#date");
-let currentTime = new Date();
-dateElement.innerHTML = formatDate(currentTime);
-
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
-
-let fahrenheitLink = document.querySelector("#fahrenheit");
-fahrenheitLink.addEventListener("click", convertFahrenheit);
-
-let celsiusLink = document.querySelector("#celsius");
-celsiusLink.addEventListener("click", convertCelsius);
-
- 
+    temperatureElement.innerHTML = 19;
+  }
+  
+  // Feature #1
+  let dateElement = document.querySelector("#date");
+  let currentTime = new Date();
+  dateElement.innerHTML = formatDate(currentTime);
+  
+  // Feature #2
+  let searchForm = document.querySelector("#search-form");
+  searchForm.addEventListener("submit", search);
+  
+  // Bonus Feature
+  let fahrenheitLink = document.querySelector("#fahrenheit");
+  fahrenheitLink.addEventListener("click", convertToFahrenheit);
+  
+  let celsiusLink = document.querySelector("#celsius");
+  celsiusLink.addEventListener("click", convertToCelsius);
+  
